11f7d2d65f0d4e6b747b3cbb497427a3d454484a
Chris Banwarth <github@aprettycoolprogram.com>
2024-05-30 12:41:29 -0400

    Prototype b240530-stable.01

--- src/Module/OpenIncident/Action/VerifyAuthor.Parse.cs
+++ src/Module/OpenIncident/Action/VerifyAuthor.Parse.cs
@@ -1,15 +1,38 @@
-﻿// u240528.1744
+﻿// u240530.1242
 
 using Outpost31.Core.Session;
 
 namespace Outpost31.Module.OpenIncident.Action
 {
+    /// <summary>Logic for the <b>VerifyAuthor</b> Command of the OpenIncident Module.</summary>
+    /// <remarks>
+    ///   <para>
+    ///    This class contains the methods that does things with the "VerifyAuthor" Command.
+    ///    The Service Command is parsed in the <b>ParseScriptCommand.cs</b> file.
+    ///  </para>
+    /// </remarks>
     public static partial class VerifyAuthor
     {
+        /// <summary>Parses the script Action for the OpenIncident Module's "VerifyAuthor" Command.</summary>
+        /// <remarks>
+        ///  <para>
+        ///   Valid VerifyAuthor Actions:
+        ///   <list type="table">
+        ///     <item>
+        ///      <term>IsOriginal</term>
+        ///      <description>Verify the Avatar user is the original author of the Open Incident.</description>
+        ///     </item>
+        ///     <item>
+        ///      <term>SaveOriginal</term>
+        ///      <description>Save the value in the "Person Completing Incident Form" field to a temporary file.</description>
+        ///     </item>
+        ///   </list>
+        ///  </para>
+        /// </remarks>
         public static void ParseAction(TingenSession tnSession)
         {
-            //Outpost31.Core.Debuggler.Primeval.Log($"[Outpost31.Module.Admin.Action.Service.ParseAction()]"); /* <- For development use only */
-            Outpost31.Core.Debuggler.Primeval.Log($"[zz]");
+            //Outpost31.Core.Debuggler.Primeval.Log($"[Outpost31.Module.OpenIncident.Action.VerifyAuthor.ParseAction()]"); /* <- For development use only */
+
             switch (tnSession.AvComponents.ScriptAction)
             {
                 case "isoriginal":
--- src/Module/OpenIncident/ParseScriptCommand.cs
+++ src/Module/OpenIncident/ParseScriptCommand.cs
@@ -1,15 +1,17 @@
-﻿// u240528.1744
+﻿// u240530.1231
 
 using Outpost31.Core.Session;
 
 namespace Outpost31.Module.OpenIncident
 {
+    /// <summary>Contains the logic for parsing the script Command for the OpenIncident Module.</summary>
     public static class ParseScriptCommand
     {
+        /// <summary>Parses the script Command.</summary>
         public static void ParseCommand(TingenSession tnSession)
         {
             //Outpost31.Core.Debuggler.Primeval.Log($"[Outpost31.Module.OpenIncident.ParseScriptCommand()]"); /* <- For development use only */
-            Outpost31.Core.Debuggler.Primeval.Log($"[001]");
+            
             switch (tnSession.AvComponents.ScriptCommand)
             {
                 case "verifyauthor":
@@ -20,6 +22,5 @@ namespace Outpost31.Module.OpenIncident
                     break;
             }
         }
-
     }
 }
