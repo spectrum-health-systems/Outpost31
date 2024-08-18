d70c0c340f4b5433fbdd363974cf0439afe82e8c
Chris Banwarth <github@aprettycoolprogram.com>
2024-08-18 10:24:38 -0400

    24.8.0-development+240818.1024

--- src/Core/Avatar/AvatarData.cs
+++ src/Core/Avatar/AvatarData.cs
@@ -5,15 +5,15 @@ using ScriptLinkStandard.Objects;
 
 namespace Outpost31.Core.Avatar
 {
-    /// <summary>Avatar-specific data and logic.</summary>
+    /// <summary>The Tingen session component that contains Avatar-related data.</summary>
     /// <include file='XmlDoc/Outpost31.Core.Avatar_doc.xml' path='Outpost31/Cs[@name="AvatarData"]/AvatarData/*'/>
     public class AvatarData
     {
-        /// <summary>The System Code that Tingen will use.</summary>
+        /// <summary>The Avatar System Code that Tingen will use.</summary>
         /// <include file='XmlDoc/Common_doc.xml' path='Common/Term[@name="Term"]/SystemCode/*'/>
         public string SystemCode { get; set; }
 
-        /// <summary>The original OptionObject sent from Avatar.</summary>
+        /// <summary>The original <see cref="OptionObject"/> sent from Avatar.</summary>
         /// <include file='XmlDoc/Common_doc.xml' path='Common/Term[@name="Term"]/SentOptionObject/*'/>
         public OptionObject2015 SentOptionObject { get; set; }
 
@@ -33,7 +33,7 @@ namespace Outpost31.Core.Avatar
         /// <param name="sentOptionObject">The <see cref="SentOptionObject"/> sent from Avatar.</param>
         /// <param name="sentScriptParameter">The <see cref="SentScriptParameter"/> sent from Avatar.</param>
         /// <returns>All of the data/information Tingen needs in order to do work.</returns>
-        /// <include file='XmlDoc/Outpost31.Core.Avatar_doc.xml' path='Outpost31/Cs[@name="ReturnObject"]/Finalize/*'/>
+        /// <include file='XmlDoc/Outpost31.Core.Avatar_doc.xml' path='Outpost31/Cs[@name="AvatarData"]/BuildObject/*'/>
         public static AvatarData BuildObject(OptionObject2015 sentOptionObject, string sentScriptParameter)
         {
             /* Trace logs cannot be used here. For debugging purposes, use a Primeval log. */
--- src/WelcomeToOutpost31.cs
+++ src/WelcomeToOutpost31.cs
@@ -3,20 +3,28 @@
 // https://github.com/APrettyCoolProgram/Outpost31
 // Copyright (c) A Pretty Cool Program. All rights reserved.
 // Licensed under the Apache 2.0 license.
-// ================================================================ 240817 =====
+// ================================================================ 240818 =====
 
-/* ---------------
- * About Outpost31
- * ---------------
- * Outpost31 is the logic for the Tingen web service.
- */
-
-// u240817.1018_code
-// u240817.1018_documentation
+// u240818.0959_code
+// u240818.0959_documentation
 
 namespace Outpost31
 {
-    /// <summary>Just a welcome message.</summary>
+    /// <summary>Welcome to Outpost31!</summary>
+    /// <remarks>
+    ///     <para>
+    ///         Outpost31 is the logic for the Tingen web service.<br/>
+    ///         <br/>
+    ///         This class is just a placeholder for documentation, and where the Outpost31<br/>
+    ///         version number is updated (see the header above).<br/>
+    ///         <br/>
+    ///         While you're here, take a look at this stuff:
+    ///         <list type="bullet">
+    ///             <item>The <see href="https://github.com/spectrum-health-systems/Tingen-Documentation">Tingen Documentation</see></item>
+    ///         </list>
+    ///         Uh...I guess that's it. Thanks for stopping by!
+    ///     </para>
+    /// </remarks>
     internal class WelcomeToOutpost31
     {
         // Nothing is done here, it's just for documentation.
